5a1a49c379ee989c9c7772959bc53a1c419355b7
GustavoPBrum <brumtavo@gmail.com>
2025-08-04 10:04:00 +0000

    Aula 91 - RequiredArgsConstructor para injecoes de dependencias

--- src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
@@ -6,6 +6,7 @@ import io.github.cursodsousa.libraryapi.exceptions.OperacaoNaoPermitidaException
 import io.github.cursodsousa.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.cursodsousa.libraryapi.model.Autor;
 import io.github.cursodsousa.libraryapi.service.AutorService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +21,10 @@ import java.util.stream.Collectors;
 
 @RestController  // Quando queremos transformar uma classe Spring em um controlador Rest
 @RequestMapping("/autores")  //http://localhost:8080/autores  --> URL que este Controller vai ficar escutando
+@RequiredArgsConstructor
 public class AutorController {
 
     private final AutorService service;
-
-    public AutorController(AutorService service) {
-        this.service = service;
-    }
 
     //@RequestMapping(method = RequestMethod.POST)  // Ou desta forma com mais parametros
     @PostMapping
--- src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
+++ src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
@@ -5,6 +5,7 @@ import io.github.cursodsousa.libraryapi.model.Autor;
 import io.github.cursodsousa.libraryapi.repository.AutorRepository;
 import io.github.cursodsousa.libraryapi.repository.LivroRepository;
 import io.github.cursodsousa.libraryapi.validator.AutorValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,20 +13,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor  // Cria um CONSTRUTOR com todas as propriedades obrigatorias (final) e cria em tempo de comp.
 public class AutorService {
 
     private final AutorRepository repository;
-
     private final AutorValidator validator;
-
     private final LivroRepository livroRepository;  // Final quer dizer que quer inicializar no construtor
 
-    // Se colocar um Bean gerenciado no construtor, o Spring vai injetar automaticamente
-    public AutorService(AutorRepository repository, AutorValidator validator, LivroRepository livroRepository) {
-        this.repository = repository;
-        this.validator = validator;
-        this.livroRepository = livroRepository;
-    }
 
     public Autor salvar(Autor autor){
         validator.validar(autor);
