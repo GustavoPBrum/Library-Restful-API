a647d478a116fa3539a5694bdeb88215e2f00393
GustavoPBrum <brumtavo@gmail.com>
2025-08-05 08:23:00 +0000

    Aula 93 - Exception Handler Validacao

---
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package io.github.cursodsousa.libraryapi.controller.common;
+
+import io.github.cursodsousa.libraryapi.controller.dto.ErroCampo;
+import io.github.cursodsousa.libraryapi.controller.dto.ErroResposta;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice  //O objetivo dessa annotation eh capturar exceptions e dar uma resposta (seja sucess ou error)
+public class GlobalExceptionHandler {
+
+    // Funciona como se fosse try/catch e colocamos o que seria o catch, o objeto que queremos retornar
+    @ExceptionHandler(MethodArgumentNotValidException.class)  // O que faz capturar o erro e jogar no parametro
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)  // Com isso mapeamos o retorno do metodo (codigo)
+    public ErroResposta handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<ErroCampo> listaErros = fieldErrors
+                .stream()
+                .map(fe -> new ErroCampo(fe.getField(), fe.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ErroResposta(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de Validacao.",
+                listaErros);
+    }
+}
