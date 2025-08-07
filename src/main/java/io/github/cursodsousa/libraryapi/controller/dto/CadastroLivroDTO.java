ed8a367a9a30765434f42820a574e1b809ea0672
GustavoPBrum <brumtavo@gmail.com>
2025-08-07 10:04:00 +0000

    Aula 98 - Metodo Post Livros

--- src/main/java/io/github/cursodsousa/libraryapi/controller/dto/CadastroLivroDTO.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/dto/CadastroLivroDTO.java
@@ -1,15 +1,26 @@
 package io.github.cursodsousa.libraryapi.controller.dto;
 
 import io.github.cursodsousa.libraryapi.model.GeneroLivro;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record CadastroLivroDTO(String isbn,
-                               String titulo,
-                               LocalDate dataPublicacao,
-                               GeneroLivro generoLivro,
-                               BigDecimal preco,
-                               UUID idAutor) {
+public record CadastroLivroDTO(
+        @ISBN
+        @NotBlank(message = "campo obrigatorio")
+        String isbn,
+        @NotBlank(message = "campo obrigatorio")
+        String titulo,
+        @NotNull(message = "campo obrigatorio")
+        @Past(message = "nao pode ser uma data futura")
+        LocalDate dataPublicacao,
+        GeneroLivro generoLivro,
+        BigDecimal preco,
+        @NotNull(message = "campo obrigatorio")
+        UUID idAutor) {
 }
