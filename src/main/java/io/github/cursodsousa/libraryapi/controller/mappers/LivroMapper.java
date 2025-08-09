2adb57ad0834642a5434f83b58732d88c68128c6
GustavoPBrum <brumtavo@gmail.com>
2025-08-09 11:24:00 +0000

    Aula 104 - MapStruct, Composicao de outro Mapper

--- src/main/java/io/github/cursodsousa/libraryapi/controller/mappers/LivroMapper.java
+++ src/main/java/io/github/cursodsousa/libraryapi/controller/mappers/LivroMapper.java
@@ -1,13 +1,15 @@
 package io.github.cursodsousa.libraryapi.controller.mappers;
 
 import io.github.cursodsousa.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.cursodsousa.libraryapi.controller.dto.ResultadoPesquisaLivroDTO;
 import io.github.cursodsousa.libraryapi.model.Livro;
 import io.github.cursodsousa.libraryapi.repository.AutorRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
+// Quero utilizar outro mapper para fazer o mapeamento (AutorMapper para mapear a entidade Autor)
+@Mapper(componentModel = "spring", uses = AutorMapper.class)  // Pode utilizar AutorMapper onde precisar nos mapeamentos
 public abstract class LivroMapper {  // Permite utilizacao de classes abstratas
 
     @Autowired
@@ -15,4 +17,7 @@ public abstract class LivroMapper {  // Permite utilizacao de classes abstratas
 
     @Mapping(target = "autor", expression = "java( repository.findById(dto.idAutor()).orElse(null) )")
     public abstract Livro toEntity(CadastroLivroDTO dto);
+
+    // ResultadoPesquisaLivroDTO precisa de um autorDTO para retornar e livro possui apenas a entidade Autor.
+    public abstract ResultadoPesquisaLivroDTO toDTO(Livro livro);
 }
