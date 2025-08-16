b04d4ce60608bd8131716c2edb0dc06bde210ae6
GustavoPBrum <brumtavo@gmail.com>
2025-08-16 15:56:00 +0000

    Aula 117 - Config SecurityFilterChain (Bean) e deixando Padrao

---
+++ src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package io.github.cursodsousa.libraryapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity  // Por ser uma config de seguranca
+public class SecurityConfiguration {
+
+    // Quando declarado esse Bean, desabilita o @Bean padrao e passa a atender as config deste Bean
+    @Bean
+    // Declarado esse SecurityFilterChain (bean), ele sobrescreve o SecurityFilterChain padrao (que habilitou o form de
+    // login, autenticacao Basic...)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)  // Permite que outras app facam uma REQUISICAO pro sistema
+                // autenticacao via browser
+                .formLogin(Customizer.withDefaults())  // Formlogin sem config extra, ele add o formulario padrao
+                // Postman
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authoize -> {  // Qualquer Request pra essa API TEM que estar Autenticado
+                    authoize.anyRequest().authenticated();
+                })
+                .build();  // Para criar um SecurityFilterChain apartir do htpp, preciso chamar o *.build*
+    }
+}
