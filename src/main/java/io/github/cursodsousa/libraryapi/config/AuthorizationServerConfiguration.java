39a928f530178a33ff04eff6131fb40c530aab65
GustavoPBrum <brumtavo@gmail.com>
2025-09-13 19:28:00 +0000

    Aula 149 - Configurando Token JWK e JWT no Authorization Server

--- src/main/java/io/github/cursodsousa/libraryapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/io/github/cursodsousa/libraryapi/config/AuthorizationServerConfiguration.java
@@ -1,5 +1,10 @@
 package io.github.cursodsousa.libraryapi.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,6 +13,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -15,7 +21,12 @@ import org.springframework.security.oauth2.server.authorization.settings.OAuth2T
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.UUID;
 
 // A configuração de um Authorization Server nada mais é do que a configuração de um SecurityFilterChain
 
@@ -64,5 +75,37 @@ public class AuthorizationServerConfiguration {
         return ClientSettings.builder()
                 .requireAuthorizationConsent(false)  // Sem tela de consentimento para acessar as informações
                 .build();
+    }
+
+    @Bean
+    // JWK (JSON Web Key) é pra gerar o Token JWT! Representação em JSON de uma chave criptográfica!
+    public JWKSource<SecurityContext> jwkSource() throws Exception {
+        // Tipo de chave criptográfica usada em criptografia assimétrica
+        RSAKey rsaKey = gerarChaveRSA();
+
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    // Método auxiliar, gera par de chaves RSA
+    private RSAKey gerarChaveRSA() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair(); // Gerando par de chaves
+
+        RSAPublicKey chavePublica = (RSAPublicKey) keyPair.getPublic();  // Classe genérica
+        RSAPrivateKey chavePrivada = (RSAPrivateKey) keyPair.getPrivate();
+
+        return new RSAKey
+                .Builder(chavePublica)
+                .privateKey(chavePrivada)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        // Passamos o JWK para ser o Decoder dos nossos Tokens
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
     }
 }
